2874d7f232d2e67a0a328922df29f57e60b94b3e
Nikola Prokopic <nikola@giantswarm.io>
2019-12-18 11:22:05 +0100

    Fixed unused parameter for Todo collector creation

--- service/collector/todo.go
+++ service/collector/todo.go
@@ -1,7 +1,10 @@
 package collector
 
 import (
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
 	"github.com/prometheus/client_golang/prometheus"
+	"k8s.io/client-go/kubernetes"
 )
 
 const (
@@ -22,13 +25,26 @@ var (
 )
 
 type TodoConfig struct {
+	K8sClient kubernetes.Interface
+	Logger    micrologger.Logger
 }
 
 type Todo struct {
+	K8sClient kubernetes.Interface
+	Logger    micrologger.Logger
 }
 
 func NewTodo(config TodoConfig) (*Todo, error) {
-	r := &Todo{}
+	if config.K8sClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.K8sClient must not be empty", config)
+	}
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
+	}
+	r := &Todo{
+		K8sClient: config.K8sClient,
+		Logger: config.Logger,
+	}
 
 	return r, nil
 }
